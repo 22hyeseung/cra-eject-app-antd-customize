5db4dac80a0674ce54b5337ddd682018c063891b
huusz <irinso93@gmail.com>
2018-02-28 16:46:33 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <Button type="primary">button</Button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
